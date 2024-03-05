650bdf7a646212d8226f5d96d0f0429c48fe799d
Mohamed <mohamed@localhost.localdomain>
2024-03-05 17:28:04 +0200

    add tables level and records level menues

--- DBMS.sh
+++ DBMS.sh
@@ -9,7 +9,99 @@ then
 fi
 
 
-function database_level {
+
+function Records_level {
+while true
+do
+	echo --------------------------------- 
+	echo 1. Insert
+	echo 2. Delete
+	echo 3. Update
+	echo 4. Select
+	echo 5. Disconnect
+	echo 6. Exit
+	echo ---------------------------------  
+
+	read -p "Select an Option, from [1-5]: " option
+
+	if [ $option -eq 1 ]
+	then
+		echo $1
+	elif [ $option -eq 2 ]
+	then
+		echo $1
+	elif [ $option -eq 3 ]
+	then
+		echo $1
+	elif [ $option -eq 4 ]
+	then
+		echo $1
+	elif [ $option -eq 5 ]
+	then
+		return
+	elif [ $option -eq 6 ]
+	then
+		exit
+	else 
+		echo not a valid option, you must select from the provided list of options, from [1-7].
+	fi
+done
+}
+
+
+function Tables_level {
+while true
+do
+	echo --------------------------------- 
+	echo 1. List all Tables
+	echo 2. Create new table  
+	echo 3. Delete an Existing Table
+	echo 4. Show content of an Existing Table
+	echo 5. Open an Existing Table 
+	echo 6. Disconnect
+	echo 7. Exit
+	echo ---------------------------------  
+
+	read -p "Select an Option, from [1-5]: " option
+
+	if [ $option -eq 1 ]
+	then
+		echo $1
+	elif [ $option -eq 2 ]
+	then
+		echo $1
+	elif [ $option -eq 3 ]
+	then
+		echo $1
+	elif [ $option -eq 4 ]
+	then
+		echo $1
+	elif [ $option -eq 5 ]
+	then
+	
+		#################
+	    	## Records Level
+		#################
+		
+		
+		
+		Records_level $1/$table_name
+		
+	elif [ $option -eq 6 ]
+	then
+		return
+		
+	elif [ $option -eq 7 ]
+	then
+		exit
+	else 
+		echo not a valid option, you must select from the provided list of options, from [1-7].
+	fi
+done
+}
+
+
+
 while true
 do
 	echo --------------------------------- 
@@ -27,7 +119,7 @@ do
 	
 		read -p "Enter Database Name: " newDB
 		
-		if [[ $newDB =~ ^[a-zA-Z0-9_]+$ ]]
+		if [[ $newDB =~ ^[a-zA-Z]+[a-zA-Z0-9_]+$ ]]
 		then
 			if ! [ -d $dbms_path/$newDB ]
 			then
@@ -37,7 +129,7 @@ do
 				echo the database $newDB is already exist !!!
 			fi
 		else
-			echo "invalid database name. The allowed characters are [ A-Z | a-z | 0-9 | _ ]".
+			echo "invalid database name. The allowed characters are [ A-Z | a-z | 0-9 | _ ]. And the name must start with at least one char".
 		fi
 		
 	elif [ $option -eq 2 ]
@@ -45,17 +137,24 @@ do
 	
 		read -p "Enter Database Name: " currentDB
 		
-		if [[ $currentDB =~ ^[a-zA-Z0-9_]+$ ]]
+		if [[ $currentDB =~ ^[a-zA-Z]+[a-zA-Z0-9_]+$ ]]
 		then
 			if [ -d $dbms_path/$currentDB ]
 			then
 			    	echo successfully connected to $currentDB.
+			    	
+			    	#################
+			    	## Tables Level
+			    	#################
+			    	
+			    	Tables_level $currentDB
+			    	
 			    	break
 			else
 				echo the database $currentDB is not exist !!!
 			fi
 		else
-			echo "invalid database name. The allowed characters are [ A-Z | a-z | 0-9 | _ ]".
+			echo "invalid database name. The allowed characters are [ A-Z | a-z | 0-9 | _ ]. And the name must start with at least one char".
 		fi
 		
 	elif [ $option -eq 3 ]
@@ -63,7 +162,7 @@ do
 	
 		read -p "Enter Database Name: " DBName
 		
-		if [[ $DBName =~ ^[a-zA-Z0-9_]+$ ]]
+		if [[ $DBName =~ ^[a-zA-Z]+[a-zA-Z0-9_]+$ ]]
 		then
 			if [ -d $dbms_path/$DBName ]
 			then
@@ -73,7 +172,7 @@ do
 				echo the database $DBName is not exist !!!
 			fi
 		else
-			echo "invalid database name. The allowed characters are [ A-Z | a-z | 0-9 | _ ]".
+			echo "invalid database name. The allowed characters are [ A-Z | a-z | 0-9 | _ ]. And the name must start with at least one char".
 		fi
 		
 		
@@ -84,16 +183,18 @@ do
 		
 	elif [ $option -eq 5 ]
 	then
-	
-		return
-		
+		exit
 	else 
 		echo not a valid option, you must select from the provided list of options, from [1-5].
 	fi
 done
-}
 
-database_level 
+
+
+
+
+
+
 
 
 
